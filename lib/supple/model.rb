20ac09464fbc777f1221b4d035b7a01d53d2ff78
GeorgeErickson <george55@mit.edu>
2014-03-03 02:20:23 -0500

    Stuff

--- lib/supple/model.rb
+++ lib/supple/model.rb
@@ -1,8 +1,18 @@
-require 'supple/model/reindex'
-
 module Supple
   module Model
     extend ActiveSupport::Concern
-    include Reindex
+    included do
+      after_commit :reindex
+    end
+
+    module ClassMethods
+      def index_name
+        @index_name ||= [table_name, Rails.env].join('_')
+      end
+
+      def reindex
+        puts 'fuck'
+      end
+    end
   end
 end
